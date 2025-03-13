1fbc5f0feffabd6b44a6610a762241ec64b2dadd
Zhouplayer <zxc1661831879@gmail.com>
2025-03-13 15:02:24 -0700

    add birthday routes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, request, url_for, send_from_directory
 from flask_cors import CORS
 from data.projects import projects_data
+from data.birthday_data import birthday_data
+import os
+
+# 配置静态文件存放路径
+MUSIC_DIR = "./data/musics"
+IMAGE_DIR = "./data/images"
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])  # Allow cross-domain requests
@@ -11,11 +17,13 @@ def hello():
     return jsonify({"message": "Hello from Flask!"})
 
 
+# get all the projects
 @app.route("/api/projects", methods=["GET"])
 def projects():
     return jsonify({"projects": projects_data})
 
 
+# get one specific project
 @app.route("/api/projects/<id>", methods=["GET"])
 def get_project_by_id(id):
     project = None
@@ -29,6 +37,7 @@ def get_project_by_id(id):
         return jsonify({"error": "Project not found"}), 404
 
 
+# get pdf resume
 @app.route("/api/resume", methods=["GET"])
 def resume_file():
     try:
@@ -41,6 +50,54 @@ def resume_file():
     except FileNotFoundError:
         # if not found, return 404
         return jsonify({"error": "Resume file not found"}), 404
+
+
+# 获取人物生日相关信息
+@app.route("/api/birthday", methods=["POST"])  # 使用 POST
+def get_birthday_music():
+    data = request.json  # 获取 POST 请求的 JSON 数据
+    friend_name = data.get("friend_name")  # 解析 friend_name 参数
+
+    if not friend_name:
+        return jsonify({"error": "Missing friend_name"}), 400
+
+    if friend_name in birthday_data:
+        person_info = birthday_data[friend_name]
+        music_filename = person_info["music_path"]
+        cake_filename = person_info["cake_path"]
+        carousel_filename = person_info["carousel_path"]
+        date = person_info["date"]
+        name = person_info["name"]
+        message = person_info["message"]
+
+        return jsonify(
+            {
+                "date": date,
+                "name": name,
+                "message": message,
+                "music": url_for(
+                    "serve_music", filename=music_filename, _external=True
+                ),
+                "cake": url_for("serve_image", filename=cake_filename, _external=True),
+                "carousel": url_for(
+                    "serve_image", filename=carousel_filename, _external=True
+                ),
+            }
+        )
+    else:
+        return jsonify({"error": f"No birthday data found for {friend_name}"}), 404
+
+
+# 提供音乐文件
+@app.route("/music/<path:filename>")
+def serve_music(filename):
+    return send_from_directory(MUSIC_DIR, filename)
+
+
+# 提供图片文件（蛋糕 & 旋转木马）
+@app.route("/images/<path:filename>")
+def serve_image(filename):
+    return send_from_directory(IMAGE_DIR, filename)
 
 
 if __name__ == "__main__":
---
+++ data/birthday_data.py
@@ -0,0 +1,19 @@
+# birthday data
+birthday_data = {
+    "TangJinglan": {
+        "music_path": "happy-birthday.mp3",
+        "cake_path": "cake.jpeg",
+        "carousel_path": "carousel.jpeg",
+        "date": "2000/03/16",
+        "name": "🎉 生日快乐, Emii姐 ! 🎂",
+        "message": "新的一岁，祝你：卡里不缺钱，晚上不失眠，火锅不长肉，快递不拖延！",
+    },
+    "Exanple": {
+        "music_path": "./data/musics/happy-birthday.mp3",
+        "cake_path": "./data/images/cake.jpeg",
+        "carousel_path": "./data/images/cake.jpeg",
+        "date": "2000/03/13",
+        "name": "🎉 生日快乐, Emii姐 ! 🎂",
+        "message": "新的一岁，祝你：卡里不缺钱，晚上不失眠，火锅不长肉，快递不拖延！",
+    },
+}
